2623b19c9b2e2a0ec0e391a8afe1279b4443ec58
Fiend21 <84176830+Fiend21@users.noreply.github.com>
2022-06-22 21:10:52 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <TodoList/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import TodoList from './components/Todo';
+
+export default {
+  name: 'App',
+
+  components: {
+    TodoList,
+},
+
+  data: () => ({
+    drawer: null, 
+  }),
+};
+</script>
---
+++ src/components/Todo.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-data-table
+        :headers="headers"
+        :items="todos"
+        :item-key="id"
+        class="-1"
+    ></v-data-table>
+</template>
+
+<script>
+    let id = 0
+    export default {
+        name: 'TodoList',
+
+        data() {
+            return {
+                headers: [
+                    {text: 'TODO',
+                     value: 'title',
+                    }
+                ],
+                todos: [
+                    {id: id++, title: 'Learn Vue'},
+                    {id: id++, title: 'Learn Vuetify'},
+                ]
+            }
+        }
+    }
+</script>
