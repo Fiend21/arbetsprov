dc83fbae6f84f9641e7075b3f5332380181fe5e7
Jocke <joakimfritz@hotmail.com>
2022-06-27 16:16:16 +0200

    First

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-toolbar-title>TodoList v.1.0</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <TodoList/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import TodoList from './components/TodoList';
+
+export default {
+  name: 'App',
+
+  components: {
+    TodoList,
+},
+
+  data: () => ({
+    drawer: null, 
+  }),
+};
+</script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,70 @@
+<template>
+    <v-data-table :headers="headers" :items="todos" :item-key="id" :items-per-page="15" class="-1" :search="search">
+        <template v-slot:top>
+            <v-text-field id="search" v-model="search" placeholder="Find TODOS..."></v-text-field>
+            <v-btn>Apply</v-btn>
+        </template>
+
+        <template v-slot:item.completed="{ item }">
+            <v-simple-checkbox v-model="item.completed" disabled></v-simple-checkbox>
+        </template>
+        <template v-slot:item.actions="{ item }">
+            <v-icon small class="mr-2" @click="editItem(item)">mdi-pencil</v-icon>
+            <v-icon small @click="deleteItem(item)">mdi-delete</v-icon>
+        </template>
+    </v-data-table>
+</template>
+
+<script>
+let id = 0
+
+export default {
+    name: 'TodoList',
+
+    data() {
+        return {
+            headers: [
+                {
+                    text: 'Description',
+                    sortable: true,
+                    value: 'description',
+                    align: 'Start',
+                },
+
+                {
+                    text: 'Created',
+                    sortable: true,
+                    value: 'created',
+                },
+
+                {
+                    text: 'Deadline',
+                    value: 'deadline',
+                },
+
+                {
+                    text: 'Completed',
+                    value: 'completed',
+                },
+
+                {
+                    sortable: false,
+                    value: 'actions',
+                }
+            ],
+            todos: [
+                { id: id++, created: '2022-06-23', description: 'Learn Vue.', deadline: '2022-06-29', completed: false },
+                { id: id++, created: '2022-06-27', description: 'Learn Vuetify.', deadline: '2022-06-29', completed: false },
+                { id: id++, created: '2022-06-27', description: 'Refactor .Net-app to use dedicated validation-class.', deadline: '2022-08-07', completed: false },
+                { id: id++, created: '2022-06-28', description: 'Learn Play-framework.', deadline: '2022-06-29', completed: false }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+#search {
+    padding-left: 2.1em;
+}
+</style>
