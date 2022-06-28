56e3cfdcd0a5acc9bc91e18b26bd285c1f79be2a
Fiend21 <84176830+Fiend21@users.noreply.github.com>
2022-06-28 18:04:48 +0200

    dsa

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -36,7 +36,7 @@
             </td>
         </template>
         <template v-slot:item.actions="{ item }">
-            <v-icon small class="mr-2" @click="editItem(item)">mdi-pencil</v-icon>
+            <v-icon small class="mr-2" @click="editItem(item)">mdi-file-edit</v-icon>
             <v-icon small @click="deleteItem(item)">mdi-delete</v-icon>
         </template>
     </v-data-table>
